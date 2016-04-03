2a5d6d4c37057f6fa59d2d5ecf504fa1072c90a0
Smallpath <smallpath2013@gmail.com>
2016-04-03 21:28:29 +0800

    Init index action for models

--- src/common/model/ad.js
+++ src/common/model/ad.js
@@ -3,5 +3,9 @@
  * model
  */
 export default class extends think.model.base {
-
+  async indexAction(name){
+    let model = this.model("ad");
+    let data = await model.where({name: name}).find();
+    return data;
+  }
 }
--- src/common/model/region.js
+++ src/common/model/region.js
@@ -3,5 +3,9 @@
  * model
  */
 export default class extends think.model.base {
-
+  async indexAction(name){
+    let model = this.model("region");
+    let data = await model.where({name: name}).find();
+    return data;
+  }
 }
