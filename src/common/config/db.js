e4a1a09a6b212d86a91f93266cbf1f042535fddf
Smallpath <smallpath2013@gmail.com>
2016-04-25 15:06:51 +0800

    Refactor styles of ad submitting

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -9,11 +9,20 @@ export default {
   log_connect: true,
   adapter: {
     mysql: {
+      /*
       host: '139.129.132.60',
       port: '3306',
       database: 'ad',
       user: 'user',
       password: '75319',
+      prefix: 'car_',
+      encoding: 'utf8'
+      */
+      host: '127.0.0.1',
+      port: '3306',
+      database: 'ad',
+      user: 'root',
+      password: '',
       prefix: 'car_',
       encoding: 'utf8'
     },
