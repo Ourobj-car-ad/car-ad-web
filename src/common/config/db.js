3bd536685b22fa5b1b378c0e8554523a75eafabb
Smallpath <smallpath2013@gmail.com>
2016-04-25 15:40:06 +0800

    Fix map layout and add scroll effect to button

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -9,7 +9,6 @@ export default {
   log_connect: true,
   adapter: {
     mysql: {
-      /*
       host: '139.129.132.60',
       port: '3306',
       database: 'ad',
@@ -17,7 +16,7 @@ export default {
       password: '75319',
       prefix: 'car_',
       encoding: 'utf8'
-      */
+      /*
       host: '127.0.0.1',
       port: '3306',
       database: 'ad',
@@ -25,6 +24,7 @@ export default {
       password: '',
       prefix: 'car_',
       encoding: 'utf8'
+      */
     },
     mongo: {
 
