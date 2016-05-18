6a958c203eeb02bb2399267a477ec5d092652c7e
Smallpath <smallpath2013@gmail.com>
2016-05-18 09:31:34 +0800

    Add support for showing logs of sended ad

--- src/home/controller/adver.js
+++ src/home/controller/adver.js
@@ -51,6 +51,55 @@ export default class extends Base {
     return this.display();
   }
   
+  async logAction(){
+    let advertiserInfo = await this.session("advertiserInfo");
+    if(!advertiserInfo || !advertiserInfo.id){
+        this.assign({
+            title:"未登陆!",
+        }) 
+        return this.display('index/index')
+    }
+    
+   
+  // return this.success(threeAds);
+
+    this.assign({
+        title:"老司机网 | 广告投放记录",
+        advertiser:advertiserInfo,
+        
+    })
+    
+   let model = this.model("ad");
+   let data = await model.where({
+       advertiser_id:advertiserInfo.id
+   }).select();
+   
+   let refuseModel = this.model("ad_reject");
+   let refuseData = await refuseModel.where({
+       advertiser_id:advertiserInfo.id
+   }).select();
+   
+    let result = data.concat(refuseData);
+    result.forEach(function(item,index){
+        if(index <data.length){
+            item.status = "审核通过";
+        }else{
+            item.status = "审核拒绝"
+        }
+        item.start_time = think.datetime(item.start_time);
+    })
+    
+    this.assign({
+        result:result,
+    })
+    
+    //return this.success(result);
+    
+    return this.display();
+  }
+  
+  
+  
   async sendAction(){
     let advertiserInfo = await this.session("advertiserInfo");
     if(!advertiserInfo || !advertiserInfo.id){
